c043b7f897d356ebe5e417acfe1f7a6dcf953d28
NeerJariwala <34756730+NeerJariwala@users.noreply.github.com>
2019-12-16 16:58:09 -0500

    cleanup

--- FaceID/problem4.py
+++ FaceID/problem4.py
@@ -6,7 +6,6 @@ import numpy as np
     In this problem, you will use PCA to perform face recogition in a face image dataset.
     We assume you have already passed the unit tests in problem4.py. There will be a file named "face_pca.npy" in your folder.
     This file contains a numpy matrix of shape (400,20), which is the reduced dimensions for the 400 face images.
-    We will need to use this file in this problem.
 
     Notations:
             ---------- input data ------------------------
@@ -39,15 +38,11 @@ def compute_distance(Xp, q):
             the distance between the second image in Xp (0,1) and the query image q (1,1) is: d[1]= square_root_of( (0-1)^2 + (1-1)^2 ) = 1
             the distance between the third image in Xp (1,1) and the query image q (1,1) is: d[1]= square_root_of( (1-1)^2 + (1-1)^2 ) = 0
         So in this case, the result should be d = [1.414,1,0]
-        Hint: you could solve this problem using 3 lines of code.
     '''
     d = np.empty(np.size(Xp, 0))
     for i in range(np.size(Xp, 0)):
         d[i] = np.linalg.norm(q-Xp[i])
     return d
-
-    ''' TEST: Now you can test the correctness of your code above by typing `nosetests -v test4.py:test_compute_distance' in the terminal.  '''
-
 
 
 
@@ -71,36 +66,7 @@ def face_recognition(Xp,q):
                  and the second most similar image is the last image (ID=2), where the distance is the second smallest: 1;
             The least similar image to the query is the first image (ID=0), where the distance is the largest: 1.414;
             So in this case, the sorted id list should be [1,2,0].
-        Hint: you could solve this problem using 2 lines of code.
     '''
     d = compute_distance(Xp, q)
     ids = np.argsort(d)
     return ids.astype(int)
-
-
-    ''' TEST: Now you can test the correctness of your code above by typing `nosetests -v test4.py:test_face_recognition' in the terminal.  
-              The query image will be saved in your current folder with name 'query.jpg',
-              Then the top 10 most similar images in the dataset will be saved in your current folder with names 'result_i.jpg'
-              Because the query image that we chose is one of the images in the dataset, so if your method works correctly, the most similar image ('result_1.jpg') should be exactly the same image as the query image ('query.jpg'). You could check by opening both images and see if they are the same.
-    '''
-
-
-
-#--------------------------------------------
-
-''' TEST Problem 4: 
-        Now you can test the correctness of all the above functions by typing `nosetests -v test4.py' in the terminal.  
-
-        If your code passed all the tests, you will see the following message in the terminal:
-            ----------- Problem 4 (20 points in total)--------------------- ... ok
-            (10 points) compute_distance ... ok
-            (10 points) face_recognition ... ok
-            ----------------------------------------------------------------------
-            Ran 3 tests in 0.075s       
-            OK
-'''
-
-#--------------------------------------------
-
-
-
