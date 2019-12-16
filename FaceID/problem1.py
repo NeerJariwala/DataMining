3d8c97da46fcef686cc10466f8dc2314d3460b93
NeerJariwala <34756730+NeerJariwala@users.noreply.github.com>
2019-12-16 16:51:31 -0500

    cleanup

--- FaceID/problem1.py
+++ FaceID/problem1.py
@@ -10,19 +10,7 @@ import numpy as np
 
 #--------------------------
 def Terms_and_Conditions():
-    ''' 
-        By submitting this homework or changing this function, you agree with the following terms:
-       (1) Not sharing your code/solution with any student before and after the homework due. For example, sending your code segment to another student, putting your solution online or lending your laptop (if your laptop contains your solution or your Dropbox automatically synchronize your solution between your home computer and your laptop) to another student to work on this homework will violate this term.
-       (2) Not using anyone's code in this homework, build your own solution. For example, using some code segments from another student or online resources due to any reason (like too busy recently) will violate this term. Changing other people's code as your solution (such as changing the variable names) will also violate this term.
-       (3) When discussing with any other student about this homework, only discuss high-level ideas or using pseudo-code. Don't discuss about the solution at the code level. For example, discussing with another student about the solution of a function (which needs 5 lines of code to solve), and then working on the solution "independently", however the code of the two solutions are exactly the same, or only with minor differences  (like changing variable names) will violate this term.
-      All violations of (1),(2) or (3) will be handled in accordance with the WPI Academic Honesty Policy.  For more details, please visit: https://www.wpi.edu/about/policies/academic-integrity/dishonesty
-      Historical Data: in one year, we ended up finding 25% of the students in the class violating this term in their homework submissions and we handled ALL of these violations according to the WPI Academic Honesty Policy. 
-    '''
-    #****************************************
-    #* CHANGE CODE HERE
-    Read_and_Agree = True  #if you have read and agree with the term above, change "False" to "True".
-    #****************************************
-    return Read_and_Agree
+    return True
 
 
 #--------------------------
@@ -50,17 +38,13 @@ def compute_eigen_pairs(X):
              (2, [0,1,0]), 
              (3, [0,0,1])]
         Here P[0] represents the first eigen pair (1,[1,0,0]), where the eigen value is 1, and the eigen vector is [1,0,0]
-        Hint: you could use np.linalg.eigh() to compute the eigen pairs of a matrix. 
+
     '''
     v, e = np.linalg.eigh(X)
     Ep = []
     for vals in range(len(v)):
         Ep.append((v[vals], e[:, vals]))
     return Ep
-
-
-    ''' TEST: Now you can test the correctness of your code above by typing `nosetests -v test1.py:test_compute_eigen_pairs' in the terminal.  '''
-
 
 
 #--------------------------
@@ -96,34 +80,8 @@ def sort_eigen_pairs(Ep, order = 'ascending'):
         Ep= [(3, [0,0,1]),
              (2, [1,0,0]), 
              (1, [0,1,0])]
-        Hint: you could use np.linalg.eigh() to compute the eigen pairs of a matrix. 
     '''
     Ep.sort()
     if order == 'descending':
         Ep = Ep[::-1]
     return Ep 
-
-    ''' TEST: Now you can test the correctness of your code above by typing `nosetests -v test1.py:test_sort_eigen_pairs' in the terminal.  '''
-
-
-
-
-#--------------------------------------------
-
-''' TEST Problem 1: 
-        Now you can test the correctness of all the above functions by typing `nosetests -v test1.py' in the terminal.  
-
-        If your code passed all the tests, you will see the following message in the terminal:
-            ----------- Problem 1 (10 points in total)--------------------- ... ok
-            (5 points) compute_eigen_pairs ... ok
-            (5 points) sort_eigen_pairs ... ok
-            
-            ----------------------------------------------------------------------
-            Ran 3 tests in 0.002s            
-            OK
-'''
-
-#--------------------------------------------
-
-
-
