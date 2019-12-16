768402f3c4c96149292679ee3a786d4b273336e2
NeerJariwala <34756730+NeerJariwala@users.noreply.github.com>
2019-12-16 17:39:12 -0500

    cleanup

--- PageRank/problem3.py
+++ PageRank/problem3.py
@@ -3,12 +3,6 @@ import numpy as np
 '''
     Problem 3: PageRank algorithm (version 1) 
     In this problem, we implement a simplified version of the pagerank algorithm, which doesn't consider sink node problem or sink region problem.
-    
-    How to run the unit tests to test your code?
-    1) Test one function in a test file:
-       After implementing one function in this file (for example, compute_P), you could test the correctness of your code by typing `nosetests -v test3.py:test_compute_P' in the terminal.
-    2) Test all functions in one test file:
-       After solving all the functions in a test file (for example, test3.py), you could test the correctness of your code by typing `nosetests -v test3.py' in the terminal.
 '''
 
 #--------------------------
@@ -20,14 +14,9 @@ def compute_P(A):
         Output: 
                 P: transition matrix, a (n by n) numpy matrix of float values.  P[j][i] represents the probability of moving from node i to node j.
                    The values in each column of matrix P should sum to 1.
-        Hint: you could solve this problem using 3 lines of code.
     '''
     P = A / A.sum(axis=0)
     return P
-
-
-    ''' TEST: Now you can test the correctness of your code above by typing `nosetests -v test3.py:test_compute_P' in the terminal.  '''
-
 
 
 
@@ -40,12 +29,9 @@ def random_walk_one_step(P, x_i):
                 x_i: pagerank scores before the i-th step of random walk. a numpy vector of length n.
         Output: 
                 x_i_plus_1: pagerank scores after the i-th step of random walk. a numpy vector of length n.
-        Hint: you could solve this problem using one line of code.
     '''
     x_i_plus_1 = P.dot(x_i)
     return x_i_plus_1
-
-    ''' TEST: Now you can test the correctness of your code above by typing `nosetests -v test3.py:test_random_walk_one_step' in the terminal.  '''
 
 
 #--------------------------
@@ -60,10 +46,7 @@ def random_walk(P, x_0, max_steps=10000):
         Output: 
                 x: the final pagerank scores after multiple steps of random walk. a numpy vector of length n.
                 n_steps: the number of steps of random walk actually used (for example, if the vector x no longer changes after 3 steps of random walk, return the value 3. 
-        Hint: You could re-use the previous function (random_walk_one_step) to simulate the random walk in each step.
-              You could use np.allclose(previous_x, new_x) function to determine when to stop the random walk iterations.
-              If the value of x in the previous iteration (previous_x) is close enough to the new value of x (new_x), the stop the iteration and return the latest value of x.
-    '''
+     '''
     old_x = x_0
     x = random_walk_one_step(P, x_0)
     n_steps = 1
@@ -77,9 +60,6 @@ def random_walk(P, x_0, max_steps=10000):
             n_steps = n_steps + 1
     return x, n_steps
 
-    ''' TEST: Now you can test the correctness of your code above by typing `nosetests -v test3.py:test_random_walk' in the terminal.  '''
-
-
 
 #--------------------------
 def pagerank_v1(A):
@@ -91,7 +71,6 @@ def pagerank_v1(A):
                 A: adjacency matrix, a numpy matrix of binary values. If there is a link from node i to node j, A[j][i] =1. Otherwise A[j][i]=0 if there is no link.
         Output: 
                 x: the pagerank scores, a numpy vector of float values, such as np.array([.3, .2, .5])
-        Hint: you could solve this problem using two lines of code. Re-use the previous functions that you have implemented to solve this problem.
     '''
     # initialize the pagerank score vector
     num_nodes = A.shape[0] # get the number of nodes (n)
@@ -100,31 +79,3 @@ def pagerank_v1(A):
     x, n_steps = random_walk(P, x_0)
 
     return x
-
-
-
-
-
-
-#--------------------------------------------
-
-''' TEST ALL: 
-        Now you can test the correctness of all the above functions by typing `nosetests -v test3.py' in the terminal.  
-
-        If your code passed all the tests, you will see the following message in the terminal:
-                ---------- Problem 3 (30 points in total) ------------ ... ok
-                (10 points) compute_P ... ok
-                (5 points) random_walk_one_step ... ok
-                (10 points) random_walk ... ok
-                (5 points) pagerank_v1 ... ok
-                
-                ----------------------------------------------------------------------
-                Ran 5 tests in 0.006s
-                
-                OK
-
-'''
-
-#--------------------------------------------
-
-
